42cad293f57881c74e9382adab504267e94768a2
AMBAR PAOLA FUJARTE HERRERA <s23120231@alumnos.itsur.edu.mx>
2026-08-17 19:48:18 -0600

    Refactor main functions into separate exercises

--- Ejercicio3_Collections.kt
+++ Ejercicio3_Collections.kt
@@ -1,3 +1,4 @@
+//Ejercicio 1
 fun main() {
     val greenNumbers = listOf(1, 4, 23)
     val redNumbers = listOf(17, 2)
@@ -5,7 +6,7 @@ fun main() {
     println(totalCount)
 }
 
-
+//Ejercicio 2
 fun main() {
     val SUPPORTED = setOf("HTTP", "HTTPS", "FTP")
     val requested = "smtp"
@@ -13,7 +14,7 @@ fun main() {
     println("Support for $requested: $isSupported")
 }
 
-
+//Ejercicio 3
 fun main() {
     val number2word = mapOf(1 to "one", 2 to "two", 3 to "three")
     val n = 2
